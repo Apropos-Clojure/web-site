1e3af884a6d3b0e99a409e52afbe8d5766df4dea
Ray McDermott <ray.mcdermott@vouch.io>
2021-08-03 16:10:50 +0200

    Read JSON from CLASSPATH

--- src/server/web_site.clj
+++ src/server/web_site.clj
@@ -1,59 +1,69 @@
 (ns server.web-site
-  (:require [muuntaja.core :as m]
-            [reitit.ring :as ring]
-            [reitit.coercion.malli :as rcm]
-            [reitit.ring.coercion :as coercion]
-            [reitit.ring.malli]
-            [reitit.spec :as rs]
-            [reitit.swagger :as swagger]
-            [reitit.swagger-ui :as swagger-ui]
-            [reitit.ring.middleware.muuntaja :as muuntaja]
-            [reitit.ring.middleware.parameters :as parameters]
-            [ring.adapter.jetty :as jetty]))
+  (:require
+    [clojure.java.io :as io]
+    [muuntaja.core :as m]
+    [reitit.ring :as ring]
+    [reitit.coercion.malli :as rcm]
+    [reitit.ring.coercion :as coercion]
+    [reitit.ring.malli]
+    [reitit.spec :as rs]
+    [reitit.swagger :as swagger]
+    [reitit.swagger-ui :as swagger-ui]
+    [reitit.ring.middleware.muuntaja :as muuntaja]
+    [reitit.ring.middleware.parameters :as parameters]
+    [ring.adapter.jetty :as jetty]))
 
 (set! *warn-on-reflection* true)
 
+(defn read-edn-file
+  [file-name]
+  (try
+    (-> file-name io/resource io/as-file slurp read-string)
+    (catch Exception _)))
+
 (defn save-episode-data
-  ;; TODO: Integrate with the DB
   [{:keys [number]}]
   {:number number})
 
 (defn episode-data
-  ;; TODO: Integrate with the DB
   [{:keys [number]}]
-  {:number number})
+  (let [file-name (str "episodes/" number ".edn")]
+    (read-edn-file file-name)))
 
-(def routes
-  [["/swagger.json"
-    {:get {:no-doc  true
-           :swagger {:info {:title       "Apropos episodes API"
-                            :description "with [malli](https://github.com/metosin/malli) and reitit-ring"}
-                     :tags [{:name "episodes", :description "episodes api"}]}
-           :handler (swagger/create-swagger-handler)}}]
-   ["/api"
-    ["/episodes"
-     ["" {:swagger {:tags ["episodes"]}
-          :post    {:summary    "Persist data for the episode"
-                    :parameters {:body [:map [:number int?]]}
-                    :responses  {200 {:body [:map [:number int?]]}}
-                    :handler    (fn [request]
-                                  (some->> (get-in request [:parameters :body])
-                                           (save-episode-data)
-                                           (assoc {:status 200} :body)))}}]
-     ["/:number"
-      {:swagger {:tags ["episodes"]}
-       :get     {:summary    "Fetch data for the specific episode number"
-                 :parameters {:path [:map [:number int?]]}
-                 :responses  {200 {:body [:map [:number int?]]}}
-                 :handler    (fn [request]
-                               (some->> (get-in request [:parameters :path])
-                                        (episode-data)
-                                        (assoc {:status 200} :body)))}}]]]])
+(def swagger-route
+  ["/swagger.json"
+   {:get {:no-doc  true
+          :swagger {:info {:title       "Apropos episodes API"
+                           :description "with [malli](https://github.com/metosin/malli) and reitit-ring"}
+                    :tags [{:name "episodes", :description "episodes api"}]}
+          :handler (swagger/create-swagger-handler)}}])
+
+(def api-route
+  ["/api"
+   ["/episodes"
+    ["" {:swagger {:tags ["episodes"]}
+         :post    {:summary    "Persist data for the episode"
+                   :parameters {:body [:map [:number int?]]}
+                   :responses  {200 {:body [:map [:number int?]]}}
+                   :handler    (fn [request]
+                                 (some->> (get-in request [:parameters :body])
+                                          (save-episode-data)
+                                          (assoc {:status 200} :body)))}}]
+    ["/:number"
+     {:swagger {:tags ["episodes"]}
+      :get     {:summary    "Fetch data for the specific episode number"
+                :parameters {:path [:map [:number int?]]}
+                :responses  {200 {:body [:map [:number int?]]}}
+                :handler    (fn [request]
+                              (some->> (get-in request [:parameters :path])
+                                       episode-data
+                                       (assoc {:status 200} :body)))}}]]])
 
 (def app
   (ring/ring-handler
     (ring/router
-      routes
+      [swagger-route
+       api-route]
       {:validate rs/validate
        :data     {:coercion   rcm/coercion
                   :muuntaja   m/instance
